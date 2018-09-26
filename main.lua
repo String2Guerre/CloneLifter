8f8a22ecd9035f06c0c766f83cc4c8dd60ba93c8
Davon <tie-n-dev@ntymail.com>
2018-09-26 21:30:20 +0200

    Change image + add controls

--- main.lua
+++ main.lua
@@ -5,29 +5,38 @@ copter.x = 0
 copter.y = 0
 copter.vx = 0
 copter.vy = 0
-copter.angle = 270
-copter.img = love.graphics.newImage("img/ship.png")
+copter.speed = 6
+copter.img = love.graphics.newImage("img/copter.png")
 
 function love.load()
+  love.graphics.setBackgroundColor(1,1,1,1)
   sWidth = love.graphics.getWidth()
   sHeight = love.graphics.getHeight()
   
   copter.x = sWidth / 2
   copter.y = sHeight / 2
   
-  rad = math.rad(copter.angle)
 end
 
 function love.update(dt)
-  copter.vy = copter.vy + 0.01
+  copter.vy = copter.vy + (3 * dt)
   
+  if love.keyboard.isDown("left") then
+    copter.x = copter.x - 1
+  end
+  if love.keyboard.isDown("right") then
+    copter.x = copter.x + 1
+  end
   if love.keyboard.isDown("up") then
+    local rad = math.rad(270)
+    local forceY = math.sin(rad) * (copter.speed * dt)
     
+    copter.vy = copter.vy + forceY
   end
   
   copter.y = copter.y + copter.vy
 end
 
 function love.draw()
-  love.graphics.draw(copter.img, copter.x, copter.y, rad, 1, 1, copter.img:getWidth()/2, copter.img:getHeight()/2)
+  love.graphics.draw(copter.img, copter.x, copter.y, rad, 1, 1, copter.img:getWidth(), copter.img:getHeight()/2)
 end
